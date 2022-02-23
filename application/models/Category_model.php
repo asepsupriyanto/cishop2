c5b578c8dc40e1a2eaad0008d77b68085129811d
asepspry12 <1910631170166@student.unsika.ac.id>
2022-02-23 12:09:37 +0700

    model dan controller category

---
+++ application/models/Category_model.php
@@ -0,0 +1,36 @@
+<?php
+
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Category_model extends MY_Model
+{
+    public function getDefaultValues()
+    {
+        return [
+            'id'    => '',
+            'slug'  => '',
+            'title' => ''
+        ];
+    }
+
+    public function getValidationRules()
+    {
+        $valudationRules = [
+            [
+                'field'     => 'slug',
+                'label'     => 'Slug',
+                'rules'     => 'trim|required|callback_unique_slug'
+            ],
+            [
+                'field'     => 'title',
+                'label'     => 'Kategori',
+                'rules'     => 'trim|required'
+            ],
+        ];
+
+        return $valudationRules;
+    }
+}
+
+/* End of file Category_model.php */
